a479f913bd8c1030aa32d1c836240284d8b9e50f
Esteban Salas <estebansalas2502@gmail.com>
2024-04-22 21:15:16 -0600

    seccion 9

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'laptop-image': "url('../assets/background-form.jpg')",
+            },
+            fontFamily: {
+                poppins: "'Poppins', sans-serif",
+            },
+        },
+    },
+    plugins: [],
+};
